bb1bf205e3a75345e00c00a4a25e1f32a5abe2a5
Vincent <Vincentnguyen030@gmail.com>
2023-12-31 12:53:42 -0600

    Change defaults

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// gameplan
+
+// need to render an inputted amount of cards
+// based on the user input, we render in that specific number of cards
+    // each card will have its own id
+
+// array of current order
+
+// function to take in array and return array with same elements as passed in array but different order
+
+// usestate to rerender onclick of cards
+// useeffect will be to determine if user has pressed the same card more than once
+ // have an array of what cards user has clicked, and check each time onclick is called
+ 
