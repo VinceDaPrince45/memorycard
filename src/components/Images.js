810f83d42d49fe57102887874188c24418d1184b
Vincent <Vincentnguyen030@gmail.com>
2024-01-07 21:38:03 -0600

    Write useEffect to move api get call into

--- src/components/Images.js
+++ src/components/Images.js
@@ -4,18 +4,21 @@ function randomInt(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-async function getPokemon(array) {
-    for (let i = 0; i < 10; i++) {
-        const randomNum = randomInt(1,pokemonCount) - 1;
-        try {
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0", {mode: 'cors'});
-            const pokemonData = await response.json();
-            array.push(pokemonData.results[randomNum-1]);
-        } catch (errror) {
-            console.log("failed");
+async function getPokemon() {
+    const pokemonArray = [];
+    
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0", {mode: 'cors'});
+        const pokemonData = await response.json();
+        for (let i = 0; i < 10; i++) {
+            const randomNum = randomInt(1,pokemonCount) - 1;
+            pokemonArray.push(pokemonData.results[randomNum]);
         }
+    } catch (errror) {
+        console.log("failed");
     }
-    console.log(array);
+    console.log(pokemonArray);
+    return pokemonArray;
 }
 
 export default getPokemon;
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,13 +1,54 @@
 import getPokemon from "./Images";
+import React, { useEffect,useState } from "react";
 
-function RandomizeCards(array) {
+function shuffleArray(array) {
+    console.log(array.length)
     if (array == undefined || array.length == 0) {
         // call function to call API and get an arbitrary number of items
+        const pokemonArray = [];
+        getPokemon(pokemonArray);
+        array = pokemonArray;
+        console.log("nothing");
     }
+    else {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        console.log("shuffling")
+    }
+    console.log(array)
 }
 
+
 export default function Test() {
-    const pokemonArray = [];
-    getPokemon(pokemonArray); 
-    console.log(pokemonArray);
+    const [cards,setCards] = useState([1,2,3,4]);
+    const [selected,setSelected] = useState([]);
+    let lost = false;
+
+    useEffect(
+        () => {
+            // execute side effect
+            // check if chosen card is in selected
+                // if so, rerender so game is over
+                // else, add chosen card to selected ; setSelected([...chosen card]) and shuffle cards and setCards to shuffled
+            return () => {
+                // cleanup function
+                if (lost) {
+                    <div>Lost</div>
+                } else {
+                    cards.map((item) => {
+                        <div key={item}>item</div>
+                    })
+                }
+            }
+        },
+        [] // dependencies
+    )
 }
+// move fetch data functions into useeffect inside Test 
+// have dependencies by an array that tracks the card user clicks
+// on first render useeffect will fetch data and setstate so that it rerenders new cards
+// after that, the second dependency will be when the array of tracked cards changes, which will setstate to shufled array
